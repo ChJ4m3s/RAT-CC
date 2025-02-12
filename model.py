f4d5557eb725061b8683c6df3dbbd425353869a6
Giacomo Chiarot <gicochiarot@gmail.com>
2025-02-13 00:39:50 +0100

    Add missing type definitions and rename `n_hidden` with `units`

--- model.py
+++ model.py
@@ -4,24 +4,24 @@ from tensorflow.keras.optimizers import Adam
 from lib import *
 
 
-def rat_cc(shape: tuple, emb_size: int, n_layers, n_hidden, l):
+def rat_cc(shape: tuple, emb_size: int, n_layers: int, units: int, l: float):
     input_layer = Input(shape=(None, shape[1]), name='input')
-    embedding = Bidirectional(LSTM(n_hidden, return_sequences=True))(input_layer)
+    embedding = Bidirectional(LSTM(units, return_sequences=True))(input_layer)
     embedding = Dropout(0.2)(embedding)
 
     for _ in range(n_layers):
-        embedding = Bidirectional(LSTM(n_hidden, return_sequences=True))(embedding)
+        embedding = Bidirectional(LSTM(units, return_sequences=True))(embedding)
         embedding = Dropout(0.2)(embedding)
 
     embedding = Bidirectional(LSTM(emb_size, return_sequences=False), name="embedding")(embedding)
 
     repeat_layer = Lambda(repeat)([input_layer, embedding])
 
-    decoder = Bidirectional(LSTM(n_hidden, return_sequences=True))(repeat_layer)
+    decoder = Bidirectional(LSTM(units, return_sequences=True))(repeat_layer)
     decoder = Dropout(0.2)(decoder)
 
     for _ in range(n_layers):
-        decoder = Bidirectional(LSTM(n_hidden, return_sequences=True))(decoder)
+        decoder = Bidirectional(LSTM(units, return_sequences=True))(decoder)
         decoder = Dropout(0.2)(decoder)
 
     reconstruction_output = TimeDistributed(Dense(shape[1]), name="reconstruction")(decoder)
